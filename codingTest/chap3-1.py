f86fa38b5652d8b2f69580350f705c0b512731e3
grace j <qwer9852@naver.com>
2020-11-01 14:58:18 +0000

    change

--- codingTest/chap3-1.py
+++ codingTest/chap3-1.py
@@ -1,10 +1,10 @@
 n = 1260
 cnt = 0
 
-coin = [500,100,50,10]
+money = [500,100,50,10]
 
-for coin in coin :
-  cnt += n //coin # n(거스름돈)을 coin으로 나눈 몫을 누적!!! 
-  n %= coin
+for money in money :
+  cnt += n //money # n(거스름돈)을 money으로 나눈 몫을 누적!!! 
+  n %= money #n을 coin으로 나눈 나머지를 n으로 다시 저장 
 
 print(cnt)
