155aa600409dfc4fc30d3c2ea229b5e8165605e7
grace j <qwer9852@naver.com>
2020-11-02 14:20:08 +0000

    commit

--- codingTest/chap3-5.py
+++ codingTest/chap3-5.py
@@ -2,7 +2,7 @@ n,k = map(int, input().split())
 cnt = 0
 
 while 1 :
-    if n == 1 : #사용자가 입력한 n이 1일때 먼저 거르고 반복을 거쳐 n = 1되면 break
+    if n == 1 : #사용자가 입력한 n이 1일때 먼저 거르고 반복을 거쳐 n = 1되면 break로 빠져나오기
         break
     elif n % k == 0 :
         n /= k 
